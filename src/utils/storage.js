69fb050fc2a75cb693893037e0ae88b9b97bdd62
Trieu Thanh <tvthanh.cod@gmail.com>
2024-06-19 16:27:40 +0700

    feat(utils): add new utils directory with helper functions

---
+++ src/utils/storage.js
@@ -0,0 +1,12 @@
+export const setCartToStorage = (value) => {
+  localStorage.setItem("cart", JSON.stringify(value));
+};
+
+export const getCartFromStorage = () => {
+  const value = localStorage.getItem("cart");
+  if (!value) {
+    return [];
+  }
+
+  return JSON.parse(value);
+};
