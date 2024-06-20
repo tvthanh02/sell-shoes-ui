e95edc56fdffb5ccf09fb1cf9fd84679c9643321
Trieu Thanh <tvthanh.cod@gmail.com>
2024-06-20 17:29:49 +0700

    feat: searchkey, total product in cart global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import { RouterProvider } from "react-router-dom";
 import router from "../routes";
+import { useReducer, createContext } from "react";
+import rootReducer from "./store/rootReducer";
+import { getCartFromStorage } from "./utils/storage";
+
+const initalState = {
+  searchKey: "",
+  quantity: getCartFromStorage().length,
+};
+
+export const AppContext = createContext();
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  const [state, dispatcher] = useReducer(rootReducer, initalState);
+  return (
+    <AppContext.Provider
+      value={{
+        state,
+        dispatcher,
+      }}
+    >
+      <RouterProvider router={router} />
+    </AppContext.Provider>
+  );
 };
 
 export default App;
